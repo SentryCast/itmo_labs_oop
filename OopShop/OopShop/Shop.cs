5b2d8f37d47742140d177b675fdf00cd72aa1239
Vadim Kuznetsov <railfuner@gmail.com>
2020-11-13 17:29:24 +0300

    first commit

---
+++ OopShop/OopShop/Shop.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OopShop
+{
+    class ShopBuilder
+    {
+        private Shop shop = new Shop();
+
+        public void SetName(string shopName)
+        {
+            shop.Name = shopName;
+        }
+        public void SetAddress(string shopAddress)
+        {
+            shop.Address = shopAddress;
+        }
+
+        public void SetProduct(Product product)
+        {
+            shop.products.Add(product);
+        }
+
+        public void SetProduct(Product product, int amount, float price)
+        {
+            for(int i = 0; i < amount; i++)
+            {
+                ProductBuilder pb = new ProductBuilder();
+                pb.SetName(product.Name);
+                pb.SetPrice(price);
+                Product FinalProduct = pb.GetResult();
+                shop.products.Add(FinalProduct);
+                pb.Reset();
+            }
+        }
+
+        public Shop GetResult()
+        {
+            return this.shop;
+        }
+
+        public void Reset()
+        {
+            shop = new Shop();
+        }
+    }
+
+    class ProductBuilder
+    {
+        private Product prod = new Product();
+
+        public void SetName(string prodName)
+        {
+            prod.Name = prodName;
+        }
+
+        public void SetPrice(float prodPrice)
+        {
+            prod.Price = prodPrice;
+        }
+
+        public Product GetResult()
+        {
+            return this.prod;
+        }
+
+        public void Reset()
+        {
+            prod = new Product();
+        }
+    }
+
+    class Shop
+    {
+        public Shop()
+        {
+            ID = Guid.NewGuid();
+        }
+        public Guid ID { get; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public List<Product> products = new List<Product>();
+
+        public void PrintAllProducts()
+        {
+            Console.WriteLine($"=== ALL PRODUCTS IN {Name} ({Address}) ===");
+            foreach (var product in products)
+            {
+                Console.WriteLine($"ProductID: {product.ID}, ProductName: {product.Name}, ProductPrice: {product.Price}");
+            }
+        }
+
+        public bool ContainsProduct(Product product)
+        {
+            foreach (var prod in products)
+            {
+                if (prod.Name == product.Name)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+    }
+
+    class Product
+    {
+        public Product()
+        {
+            ID = Guid.NewGuid();
+        }
+        public Guid ID { get; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ OopShop/OopShop/ShopAnalyzer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OopShop
+{
+    class ShopAnalyzer
+    {
+        private List<Shop> shops = new List<Shop>();
+
+        public void AddShop(Shop shop)
+        {
+            shops.Add(shop);
+        }
+
+        public void PrintShopWithMinProdPrice(string name)
+        {
+            float FoundPrice = Int32.MaxValue;
+            string FoundShop = null;
+            string FoundID = null;
+            foreach(var shop in shops)
+            {
+                foreach (var product in shop.products)
+                {
+                    if (product.Name == name)
+                    {
+                        if (product.Price < FoundPrice)
+                        {
+                            FoundPrice = product.Price;
+                            FoundShop = shop.Name;
+                            FoundID = Convert.ToString(shop.ID);
+                        }
+                    }
+                }
+            }
+            if(FoundShop == null)
+                Console.WriteLine($"Have not found shop with lowest price of this product: {name}");
+            else
+                Console.WriteLine($"Shop with minimum price of {name} is {FoundShop} (product id: {FoundID}), price: {FoundPrice}");
+        }
+
+        public void WhatCanBuy(Shop shop, float cost)
+        {
+            foreach(var product in shop.products)
+            {
+                int amount = (int)(cost / product.Price);
+                if(!(amount <= 0))
+                    Console.WriteLine($"You can buy {amount} of {product.Name}");
+            }
+        }
+
+        public void BuyManyProducts(Shop shop, string productName, int productAmount)
+        {
+            int localProductAmount = 0;
+            float price = 0;
+            foreach(var product in shop.products)
+            {
+                if(product.Name == productName)
+                {
+                    localProductAmount++;
+                    price = product.Price;
+                }
+            }
+
+            if(localProductAmount >= productAmount)
+                Console.WriteLine($"You have bought {productAmount} of {productName} for {price*productAmount}");
+            else
+                Console.WriteLine($"Not enough of product {productName} in the {shop.Name} shop");
+        }
+
+        public void WhichShopHasCheaperProducts(string productName, int productAmount)
+        {
+            string bestShop = null;
+            float bestCost = Int32.MaxValue;
+            foreach(var shop in shops)
+            {
+                string shopName = shop.Name;
+                int localProductAmount = 0;
+                float price = 0;
+                foreach(var product in shop.products)
+                {
+                    if(product.Name == productName)
+                    {
+                        localProductAmount++;
+                        price = product.Price;
+                    }
+                }
+                float cost = price * localProductAmount;
+                if (cost < bestCost && cost != 0 && localProductAmount >= productAmount)
+                {
+                    bestShop = shop.Name;
+                    bestCost = cost;
+                }
+            }
+            if(bestCost != Int32.MaxValue && bestShop != null)
+                Console.WriteLine($"The best shop for you is {bestShop}, you need to pay {bestCost}");
+            else
+                Console.WriteLine("There is no suitable shops for you");
+        }
+    }
+}
